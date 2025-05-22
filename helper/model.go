7b2534788e4fff062ab81493d903bb2604c358ee
Azhar Dhiyaulhaq <dhiyazhar@gmail.com>
2025-05-22 21:20:36 +0700

    add: helper for writing restoran's response

---
+++ helper/model.go
@@ -0,0 +1,24 @@
+package helper
+
+import (
+	"nesanest-rest-api/model/domain"
+	"nesanest-rest-api/model/web"
+)
+
+func ToRestoranResponse(restoran domain.Restoran) web.RestoranResponse {
+	return web.RestoranResponse{
+		Id:          restoran.Id,
+		Name:        restoran.Name,
+		Description: restoran.Description,
+	}
+}
+
+func ToRestoranResponses(restorans []domain.Restoran) []web.RestoranResponse {
+	var restoranResponses []web.RestoranResponse
+
+	for _, restoran := range restorans {
+		restoranResponses = append(restoranResponses, ToRestoranResponse(restoran))
+	}
+
+	return restoranResponses
+}
