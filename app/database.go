4bdb8c862cd9287ebdb74e700eea7298afe56a0d
Azhar Dhiyaulhaq <dhiyazhar@gmail.com>
2025-05-22 21:22:54 +0700

    add: postgres database connection

---
+++ app/database.go
@@ -0,0 +1,39 @@
+package app
+
+import (
+	"database/sql"
+	"fmt"
+	"log/slog"
+	"nesanest-rest-api/helper"
+	"time"
+
+	_ "github.com/lib/pq"
+)
+
+func NewDB() *sql.DB {
+	host := "localhost"
+	port := 5432
+	user := "azhar"
+	password := "azhar"
+	dbname := "nesanest"
+
+	dsn := fmt.Sprintf(
+		"host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname,
+	)
+
+	db, err := sql.Open("postgres", dsn)
+	helper.PanicIfError(err)
+
+	err = db.Ping()
+	helper.PanicIfError(err)
+
+	slog.Info("Successfully connect to database")
+
+	db.SetMaxIdleConns(5)
+	db.SetMaxOpenConns(20)
+	db.SetConnMaxLifetime(60 * time.Minute)
+	db.SetConnMaxIdleTime(10 * time.Minute)
+
+	return db
+}
