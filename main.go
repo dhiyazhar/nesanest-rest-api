2d0bf39f5ca308435b50e7d33ed0520d5441b92e
Azhar Dhiyaulhaq <dhiyazhar@gmail.com>
2025-05-22 21:26:36 +0700

    change: a lot

--- main.go
+++ main.go
@@ -1,26 +1,36 @@
 package main
 
 import (
+	"log/slog"
+	"nesanest-rest-api/app"
+	"nesanest-rest-api/controller"
+	"nesanest-rest-api/helper"
+	"nesanest-rest-api/middleware"
+	"nesanest-rest-api/repository"
+	"nesanest-rest-api/service"
 	"net/http"
+
+	"github.com/go-playground/validator/v10"
+	_ "github.com/lib/pq"
 )
 
-func setJsonResp(message []byte, httpCode int, res http.ResponseWriter) {
-	res.Header().Set("Content-Type", "application/json")
-	res.WriteHeader(httpCode)
-	res.Write(message)
-}
-
-type homeHandler struct{}
-
-func (h *homeHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	message := []byte(`{"message": "Golang REST API - Azhar 2025"}`)
-	setJsonResp(message, http.StatusOK, w)
-}
-
 func main() {
-	mux := http.NewServeMux()
+	db := app.NewDB()
+	validate := validator.New()
+	restoranRepository := repository.NewRestoranRepository()
+	restoranService := service.NewRestoranService(restoranRepository, db, validate)
+	restoranController := controller.NewRestoranController(restoranService)
 
-	mux.Handle("/", &homeHandler{})
+	router := app.NewRouter(restoranController)
 
-	http.ListenAndServe(":8080", mux)
+	server := http.Server{
+		Addr:    "localhost:3000",
+		Handler: middleware.NewAuthMiddleware(router),
+	}
+
+	slog.Info("Starting server", "address", server.Addr)
+
+	err := server.ListenAndServe()
+	helper.PanicIfError(err)
+
 }
