f5420b882f38aac4eec8da0282c663b9cfcc66a9
Chanie-ago <putraabriel14@gmail.com>
2025-05-28 16:38:33 +0700

    add: integrate review handling into main application flow

--- cmd/main.go
+++ cmd/main.go
@@ -1,41 +1,46 @@
 package main
 
 import (
-	"log/slog"
-	"nesanest-rest-api/app"
-	"nesanest-rest-api/controller"
-	"nesanest-rest-api/helper"
-	"nesanest-rest-api/middleware"
-	"nesanest-rest-api/repository"
-	"nesanest-rest-api/service"
-	"net/http"
+    "log/slog"
+    "nesanest-rest-api/app"
+    "nesanest-rest-api/controller"
+    "nesanest-rest-api/helper"
+    "nesanest-rest-api/middleware"
+    "nesanest-rest-api/repository"
+    "nesanest-rest-api/service"
+    "net/http"
 
-	"github.com/go-playground/validator/v10"
-	_ "github.com/lib/pq"
+    "github.com/go-playground/validator/v10"
+    _ "github.com/lib/pq"
 )
 
 func main() {
-	db := app.NewDB()
-	validate := validator.New()
+    db := app.NewDB()
+    validate := validator.New()
 
-	restoranRepository := repository.NewRestoranRepository()
-	restoranService := service.NewRestoranService(restoranRepository, db, validate)
-	restoranController := controller.NewRestoranController(restoranService)
+    restoranRepository := repository.NewRestoranRepository()
+    restoranService := service.NewRestoranService(restoranRepository, db, validate)
+    restoranController := controller.NewRestoranController(restoranService)
 
-	userRepository := repository.NewUserRepository()
-	userService := service.NewUserService(userRepository, db)
-	userController := controller.NewUserController(userService)
+    userRepository := repository.NewUserRepository()
+    userService := service.NewUserService(userRepository, db)
+    userController := controller.NewUserController(userService)
 
-	router := app.NewRouter(restoranController, userController)
-	
-	server := http.Server{
-		Addr:    "localhost:3000",
-		Handler: middleware.NewAuthMiddleware(router),
-	}
+    // Tambahkan dependency review
+    reviewRepository := repository.NewReviewRepository()
+    reviewService := service.NewReviewService(reviewRepository, db)
+    reviewController := controller.NewReviewController(reviewService)
 
-	slog.Info("Starting server", "address", server.Addr)
+    // Inject reviewController ke router
+    router := app.NewRouter(restoranController, userController, reviewController)
 
-	err := server.ListenAndServe()
-	helper.PanicIfError(err)
+    server := http.Server{
+        Addr:    "localhost:3000",
+        Handler: middleware.NewAuthMiddleware(router),
+    }
 
+    slog.Info("Starting server", "address", server.Addr)
+
+    err := server.ListenAndServe()
+    helper.PanicIfError(err)
 }
