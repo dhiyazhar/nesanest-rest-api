2314603076336693b8988d2a8b41a43da399a4d5
Azhar Dhiyaulhaq <dhiyazhar@gmail.com>
2025-05-28 13:29:07 +0700

    add: add ID to ctx

--- helper/jwt.go
+++ helper/jwt.go
@@ -1,28 +1,29 @@
 package helper
 
 import (
-    "time"
-    "github.com/golang-jwt/jwt/v5"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
 )
 
-var jwtKey = []byte("secret_key") 
+var jwtKey = []byte("secret_key")
 
 func GenerateJWT(userId int, email string) (string, error) {
-    claims := jwt.MapClaims{
-        "user_id": userId,
-        "email":   email,
-        "exp":     time.Now().Add(24 * time.Hour).Unix(),
-    }
-    token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-    return token.SignedString(jwtKey)
+	claims := jwt.MapClaims{
+		"user_id": userId,
+		"email":   email,
+		"exp":     time.Now().Add(24 * time.Hour).Unix(),
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString(jwtKey)
 }
 
 func ParseJWT(tokenStr string) (jwt.MapClaims, error) {
-    token, err := jwt.Parse(tokenStr, func(token *jwt.Token) (interface{}, error) {
-        return jwtKey, nil
-    })
-    if err != nil || !token.Valid {
-        return nil, err
-    }
-    return token.Claims.(jwt.MapClaims), nil
+	token, err := jwt.Parse(tokenStr, func(token *jwt.Token) (interface{}, error) {
+		return jwtKey, nil
+	})
+	if err != nil || !token.Valid {
+		return nil, err
+	}
+	return token.Claims.(jwt.MapClaims), nil
 }
