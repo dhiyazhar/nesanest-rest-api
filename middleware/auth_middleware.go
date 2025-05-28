d67194b644b7177af42a49a337ab8e7580dab948
Azhar Dhiyaulhaq <dhiyazhar@gmail.com>
2025-05-28 17:41:15 +0700

    change: delete temp middleware

--- middleware/auth_middleware.go
+++ middleware/auth_middleware.go
@@ -2,36 +2,10 @@ package middleware
 
 import (
 	"context"
-	"fmt"
 	"nesanest-rest-api/helper"
-	"nesanest-rest-api/model/web"
 	"net/http"
 	"strings"
 )
-
-type AuthMiddleware struct {
-	Handler http.Handler
-}
-
-func NewAuthMiddleware(handler http.Handler) *AuthMiddleware {
-	return &AuthMiddleware{
-		Handler: handler,
-	}
-}
-
-func (middleware *AuthMiddleware) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
-	if request.Header.Get("X-API-Key") == "RAHASIA" {
-		middleware.Handler.ServeHTTP(writer, request)
-	} else {
-		writer.Header().Set("Content-Type", "application/json")
-		writer.WriteHeader(http.StatusUnauthorized)
-		webResponse := web.WebResponse{
-			Code:   http.StatusUnauthorized,
-			Status: "UNAUTHORIZED",
-		}
-		helper.WriteToResponseBody(writer, webResponse)
-	}
-}
 
 func JWTAuthMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -57,8 +31,6 @@ func JWTAuthMiddleware(next http.Handler) http.Handler {
 
 		ctx := context.WithValue(r.Context(), "user_id", int(claims["user_id"].(float64)))
 		ctx = context.WithValue(ctx, "email", claims["email"].(string))
-		ctx_debug_id := ctx.Value("user_id")
-		fmt.Printf("\nctx_debug_id DEBUG - %v", ctx_debug_id)
 
 		next.ServeHTTP(w, r.WithContext(ctx))
 	})
