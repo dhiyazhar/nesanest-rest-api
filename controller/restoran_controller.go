802f026b2d77150693ddff082b8d473d50cd89ea
Azhar Dhiyaulhaq <dhiyazhar@gmail.com>
2025-05-28 13:27:17 +0700

    change: get ID from ctx

--- controller/restoran_controller.go
+++ controller/restoran_controller.go
@@ -6,8 +6,8 @@ import (
 
 type RestoranController interface {
 	Create(writer http.ResponseWriter, request *http.Request)
-	Update(writer http.ResponseWriter, request *http.Request, id string)
-	Delete(writer http.ResponseWriter, request *http.Request, id string)
-	FindById(writer http.ResponseWriter, request *http.Request, id string)
+	Update(writer http.ResponseWriter, request *http.Request)
+	Delete(writer http.ResponseWriter, request *http.Request)
+	FindById(writer http.ResponseWriter, request *http.Request)
 	FindAll(writer http.ResponseWriter, request *http.Request)
 }
